89df94d036c2f601ee9d84f791a33954d2c20cda
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2024-03-27 18:39:57 -0400

    printing and turtle basics in chapter 1

---
+++ TurtleBasics.py
@@ -0,0 +1,16 @@
+import turtle
+
+littleTurtle = turtle.Turtle()
+
+littleTurtle.pencolor("green")
+littleTurtle.forward(100)
+littleTurtle.right(90)
+littleTurtle.forward(100)
+littleTurtle.left(75)
+littleTurtle.backward(57)
+littleTurtle.penup()
+littleTurtle.pencolor("purple")
+littleTurtle.circle(100)
+littleTurtle.pendown()
+littleTurtle.setx(50.00)
+littleTurtle.backward(90)
